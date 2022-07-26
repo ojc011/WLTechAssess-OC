366d4eef0ddfaab97ed5afd505af86bdcd29210c
Oliver C <ojc011@yahoo.com>
2022-07-26 15:10:33 -0500

    Fixed edit link, updated several styling aspects, now working on edit form

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,8 +1,29 @@
 class AddressesController < ApplicationController
+
+  def show
+    @address = Address.find(params[:id])
+  end
+
+  def edit
+    @contact = Contact.find(params[:contact_id])
+    @address = @contact.addresses.find(params[:id])
+  end
+
   def create
     @contact = Contact.find(params[:contact_id])
     @address = @contact.addresses.create(address_params)
     redirect_to contact_path(@contact)
+  end
+
+  def update
+    @contact = Contact.find(params[:contact_id])
+    @address = @contact.addresses.create(address_params)
+
+    if @address.update(address_params)
+      redirect_to @address, notice: "Saved!"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -14,6 +35,6 @@ class AddressesController < ApplicationController
 
   private
     def address_params
-      params.require(:address).permit(:street, :town, :zip, :state, :country_code)
+      params.require(:address).permit(:street, :town, :zip, :state, :country)
     end
 end
