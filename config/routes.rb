f32a7790678164ddd43412bb614b4c7abe4219b9
Oliver C <ojc011@yahoo.com>
2022-07-29 20:02:51 -0500

    some minor changes to hopefully address heroku errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,20 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
+  
   get '/signup' => 'user#new'
   post '/users' => 'user#create'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
   get 'contacts/index'
   resources :contacts do
     resources :addresses, :phone_numbers, :emails
   end
-  root to: 'user#index'
   
-  get '/' => 'sessions#create'
+  get '/' => 'user#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
