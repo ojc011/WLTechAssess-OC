297a740b593e243ef309e28dd987572adabd1790
Oliver C <ojc011@yahoo.com>
2022-07-31 14:07:36 -0500

    Removed welcome controllerrb and changed / route from get to root to

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :addresses, :phone_numbers, :emails
   end
   
-  get '/' => 'user#index'
+  root to: 'user#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
